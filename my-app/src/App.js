dfe02a85fafa94a101838f004b4518636c55b51e
Shreya-sinha <sinhashreya2679@gmail.com>
2023-09-20 23:58:36 +0530

    updated App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList.js";
+import Footer from "./Components/Footer.js";
 
 function App() {
+  const productlist = [
+    {
+      price: 99999,
+      name: "Iphone 10s Max",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi note 10s Max",
+      quantity: 0,
+    },
+  ];
   return (
     <>
       <Navbar />
+      <ProductList productList={productlist} />
+      <Footer />
     </>
   );
 }
